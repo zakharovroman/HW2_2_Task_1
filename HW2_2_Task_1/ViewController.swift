14ed55428223d2877fc602a108e13d4771c1cee9
Zakharov Roman <zakharovroman@hotmail.com>
2020-05-22 18:03:21 +0300

    v2

--- HW2_2_Task_1/ViewController.swift
+++ HW2_2_Task_1/ViewController.swift
@@ -12,11 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var redLabel: UILabel!
-    @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var redSlider: UISlider!
-    @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +28,9 @@ class ViewController: UIViewController {
     }
     
     func setColorView() {
-        redLabel.text = String(redSlider.value)
-        blueLabel.text = String(blueSlider.value)
-        greenLabel.text = String(greenSlider.value)
+        redLabel.text = String(round(100 * redSlider.value) / 100)
+        greenLabel.text = String(round(100 * greenSlider.value) / 100)
+        blueLabel.text = String(round(100 * blueSlider.value) / 100)
         colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
     }
 }
